80ecdea119d1cd7c887fa964fd8ef385d1e67d1e
Moskalev Nikita <mozgg2002@gmail.com>
2023-01-12 14:25:19 +0300

    add: more tests

--- test/main.cpp
+++ test/main.cpp
@@ -30,7 +30,43 @@ TEST(EarleyRecognizer, IncorrectBracketSequence) {
   EXPECT_FALSE(recognizer.Recognize("((()))(()"));
 }
 
-int main(int argc, char **argv) {
+TEST(EarleyRecognizer, CorrectArithmeticExpression) {
+  Grammar grammar{
+      Rule{'S', "E"},
+      std::vector<Rule>{
+          Rule{'E', "E+T"}, Rule{'E', "E-T"}, Rule{'E', "T"}, Rule{'T', "T*F"},
+          Rule{'T', "T/F"}, Rule{'T', "F"}, Rule{'F', "0"}, Rule{'F', "1"},
+          Rule{'F', "2"}, Rule{'F', "3"}, Rule{'F', "4"}, Rule{'F', "5"},
+          Rule{'F', "6"}, Rule{'F', "7"}, Rule{'F', "8"}, Rule{'F', "9"}}};
+
+  EarleyRecognizer recognizer{grammar};
+
+  EXPECT_TRUE(recognizer.Recognize("7-5"));
+  EXPECT_TRUE(recognizer.Recognize("7-5*5"));
+  EXPECT_TRUE(recognizer.Recognize("6*7-5/5"));
+  EXPECT_TRUE(recognizer.Recognize("1-3*5+5*4*5*5+5"));
+  EXPECT_TRUE(recognizer.Recognize("0+1+2+3+4+5-6-7-8-9"));
+}
+
+TEST(EarleyRecognizer, IncorrectArithmeticExpression) {
+  Grammar grammar{
+      Rule{'S', "E"},
+      std::vector<Rule>{
+          Rule{'E', "E+T"}, Rule{'E', "E-T"}, Rule{'E', "T"}, Rule{'T', "T*F"},
+          Rule{'T', "T/F"}, Rule{'T', "F"}, Rule{'F', "0"}, Rule{'F', "1"},
+          Rule{'F', "2"}, Rule{'F', "3"}, Rule{'F', "4"}, Rule{'F', "5"},
+          Rule{'F', "6"}, Rule{'F', "7"}, Rule{'F', "8"}, Rule{'F', "9"}}};
+
+  EarleyRecognizer recognizer{grammar};
+
+  EXPECT_FALSE(recognizer.Recognize("7--5"));
+  EXPECT_FALSE(recognizer.Recognize("*5*"));
+  EXPECT_FALSE(recognizer.Recognize("3+*2"));
+  EXPECT_FALSE(recognizer.Recognize("5/3-0+4*3**4"));
+  EXPECT_FALSE(recognizer.Recognize("1-3*5+5**4*5*5+5"));
+}
+
+int main(int argc, char** argv) {
   ::testing::InitGoogleTest(&argc, argv);
   return RUN_ALL_TESTS();
 }
