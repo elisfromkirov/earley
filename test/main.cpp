b496c13be7fc07978c8406b8950e7bf4db0992f3
Moskalev Nikita <mozgg2002@gmail.com>
2023-01-12 14:08:58 +0300

    fix: refactor

--- test/main.cpp
+++ test/main.cpp
@@ -1,6 +1,27 @@
-#include <earley/function.hpp>
+#include <earley/recognizer.hpp>
+
+#include <iostream>
 
 int main() {
-  Function();
+  Grammar grammar{
+      Rule{'S', "U"},
+      std::vector<Rule>{Rule{'U', "UU"}, Rule{'U', ""}, Rule{'U', "(U)"}}};
+
+  EarleyRecognizer recognizer{grammar};
+
+  std::cout << std::boolalpha;
+
+  std::cout << recognizer.Recognize("()") << std::endl;
+  std::cout << recognizer.Recognize("()()") << std::endl;
+  std::cout << recognizer.Recognize("()(()())") << std::endl;
+  std::cout << recognizer.Recognize("()((()))()") << std::endl;
+  std::cout << recognizer.Recognize("(())()(())") << std::endl;
+
+  std::cout << recognizer.Recognize("(") << std::endl;
+  std::cout << recognizer.Recognize("(()))") << std::endl;
+  std::cout << recognizer.Recognize("(())())") << std::endl;
+  std::cout << recognizer.Recognize("())((((") << std::endl;
+  std::cout << recognizer.Recognize("((()))(()") << std::endl;
+
   return 0;
 }
